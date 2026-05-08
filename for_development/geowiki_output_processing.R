a4f5863e709d235c3fb51fc5d05fed36bf91e0d8
xavi-rp <xavi.rotllan.puig@gmail.com>
2026-05-08 10:29:04 +0200

    script adapted to process 2026 geowiki data

---
+++ for_development/geowiki_output_processing.R
@@ -0,0 +1,328 @@
+
+library(tidyverse)
+
+
+
+
+
+# ---- 0) Setup ----
+dir <- "/Users/xavi_rp/Documents/JRC_D1/AccuracyAssessment_Second/kk/"
+
+input_file <- "20260504.csv"
+input_file <- "20240905.csv"
+
+input_file <- paste0(dir, input_file)
+
+
+# Extract date (YYYYMMDD)
+date_part <- tools::file_path_sans_ext(basename(input_file))
+
+
+
+
+# ---- 1) Read CSV (semicolon separated) ----
+
+input_file_df <- read.csv(input_file) # col 'name' is repeated. Transformed them into "name" and "name.1" 
+
+
+head(input_file_df)
+nrow(input_file_df)
+ncol(input_file_df)
+names(input_file_df)
+
+
+# ---- 2) Save initial copy ----
+
+write.csv(input_file_df, 
+          paste0(dir, date_part, "_GFC_2020_secondary.csv"), 
+          row.names = FALSE)
+
+
+
+# ---- 3) Remove last line ----     
+# Make sure if needed!!
+
+#View(tail(input_file_df))
+
+#input_file_df <- input_file_df %>%
+#  slice_head(n = n() - 1)
+
+
+
+
+# ---- 4 & 5) Remove commas and quotation marks ----
+View(input_file_df)
+
+# just to be sure if there are
+checks <- input_file_df %>%
+  summarise(across(
+    everything(),
+    list(
+      commas = ~ sum(str_count(.x, ","), na.rm = TRUE),
+      quotes = ~ sum(str_count(.x, '"'), na.rm = TRUE)
+    )
+  )) %>% as.data.frame()  %>% sum()
+
+
+if (checks != 0){
+  input_file_df <- input_file_df %>%
+    mutate(across(everything(), ~ str_replace_all(.x, ",", ""))) %>%
+    mutate(across(everything(), ~ str_replace_all(.x, '"', "")))
+  
+  print("commas and quotation marks removed")
+}
+
+
+
+
+
+# ---- 6) Save modified version ----
+
+write.csv(input_file_df, 
+          paste0(dir, date_part, "_GFC_2020_secondary_modified.csv"), 
+          row.names = FALSE)
+
+
+
+
+
+# ---- 7 & 8) Replace emails ----
+ # to be clarified with Rene
+input_file_df <- input_file_df %>%
+  mutate(
+    email = case_when(
+      email == "rene.colditz@ec.europa.eu" ~ "danilo.mollicone@gmail.com",
+      email == "astrid.verhegghen@ec.europa.eu" ~ "valery.gond@cirad.fr",
+      TRUE ~ email
+    )
+  )
+
+
+write.csv(input_file_df, 
+          paste0(dir, date_part, "_GFC_2020_secondary_modified.csv"), 
+          row.names = FALSE)
+
+
+
+
+
+# ---- 9) IDL code per_line_secondary.pro ----
+
+secondary_modified <- read.csv(paste0(dir, date_part, "_GFC_2020_secondary_modified.csv"))
+
+View(secondary_modified)
+
+unique(secondary_modified$name)
+unique(secondary_modified$name.1)
+
+length(unique(secondary_modified$validation_id))
+nrow(secondary_modified)
+nrow(secondary_modified)/length(unique(secondary_modified$validation_id))
+
+table(secondary_modified$name)
+
+# check if there are duplicates per (validation_id, question). If 0 rows: each question appears exactly once per validation
+secondary_modified %>%
+  count(validation_id, name) %>%
+  filter(n > 1)
+
+
+
+secondary_modified_wide <- secondary_modified %>%
+  # make column names easier to work with
+  rename(
+    question = name,
+    answer = name.1
+  ) %>%
+  # Keep all other columns as identifiers
+  pivot_wider(
+    names_from = question,
+    values_from = answer
+  )  %>%
+  relocate(
+    `GFC validation - forest`,
+    `GFC validation - confidence`,
+    `GFC validation - forest type`,
+    `GFC validation - other land use type`,
+    `GFC validation - Issues with class assignment`,
+    .after = last_col()
+  )
+
+View(secondary_modified_wide)
+
+nrow(secondary_modified_wide)
+length(unique(secondary_modified_wide$validation_id)) * 4
+
+nrow(secondary_modified_wide)/length(unique(secondary_modified_wide$validation_id))
+length(unique(secondary_modified_wide$validation_id)) * 4
+
+head(sort(table(secondary_modified_wide$validation_id)), 10)
+sum(table(secondary_modified_wide$validation_id) == 4)  # 4233
+sum(table(secondary_modified_wide$validation_id) == 3)  #   80
+
+secondary_modified_wide[secondary_modified_wide$validation_id == 2745966, ] %>% View() # if 'GFC validation - forest' = 'no assignment', it has only 3 rows
+secondary_modified_wide[secondary_modified_wide$validation_id == 2748197, ] %>% View() 
+
+
+
+### Collapsing the rows into a single row per validation_id, merging the non-NA values ####
+secondary_modified_wide_1row <- secondary_modified_wide %>%
+  group_by(validation_id) %>%
+  summarise(
+    across(starts_with("GFC"),
+           ~ if (all(is.na(.x))) NA else na.omit(.x)[1]),  # if all values are NA, keep NA
+    across(-c(starts_with("GFC")), first),
+    .groups = "drop") %>%
+  relocate(starts_with("GFC"), .after = last_col()) %>%
+  relocate(comment, .after = last_col())
+
+View(secondary_modified_wide_1row)
+
+
+secondary_modified_wide_1row[secondary_modified_wide_1row$validation_id == 2745966, ] %>% View() 
+secondary_modified_wide_1row[secondary_modified_wide_1row$validation_id == 2748197, ] %>% View() 
+
+
+
+write.csv(secondary_modified_wide_1row, 
+          paste0(dir, "secondary_data.csv"), 
+          row.names = FALSE)
+
+
+
+
+
+### Keeping the latest validation ####
+
+## are there samples which have several validations?
+
+length(unique(secondary_modified_wide$validation_id)) # 4313, because some sample units were validated more than once
+length(unique(secondary_modified_wide$sample_id))     # 4000
+
+
+
+secondary_modified_latest <- secondary_modified_wide_1row %>%
+  group_by(sample_id) %>%
+  slice_max(validation_id, n = 1, with_ties = FALSE) %>%
+  ungroup()
+
+View(secondary_modified_latest)
+
+
+
+write.csv(secondary_modified_latest, 
+          paste0(dir, "secondary_data_latest.csv"), 
+          row.names = FALSE)
+
+
+
+### Some checks ####
+
+View(secondary_modified[secondary_modified$sample_id == 2027527, ])
+View(secondary_modified_wide[secondary_modified_wide$sample_id == 2027527, ])
+View(secondary_modified_wide_1row[secondary_modified_wide_1row$sample_id == 2027527, ])  # info from "submission_item_id" and "enhancement" are lost, but they seem not relevant
+View(secondary_modified_latest[secondary_modified_latest$sample_id == 2027527, ])
+
+
+
+### Checking against published version (2025) of "secondary_data_latest.csv" ####
+
+secondary_data_latest_2025 <- read.csv(paste0("/Users/xavi_rp/Documents/JRC_D1/Files_from_Geowiki_and_our_processing/", "secondary_data_latest.csv"))
+
+sort(names(secondary_data_latest_2025))
+View(secondary_data_latest_2025)
+nrow(secondary_data_latest_2025) 
+
+head(secondary_modified_latest)
+nrow(secondary_data_latest_2025) == nrow(secondary_modified_latest)  # 4000
+
+sum(sort(unique(secondary_data_latest_2025$validation_id)) != sort(unique(secondary_modified_latest$validation_id)))     # 0
+identical(sort(unique(secondary_data_latest_2025$validation_id)), sort(unique(secondary_modified_latest$validation_id))) # TRUE
+
+identical(sort(unique(secondary_data_latest_2025$sample_id)), sort(unique(secondary_modified_latest$sample_id))) # TRUE
+
+sort(names(secondary_modified_latest))
+View(secondary_modified_latest)
+
+identical(arrange(secondary_data_latest_2025, validation_id)$name.1, arrange(secondary_modified_latest, validation_id)$`GFC validation - forest`)   # TRUE
+
+identical(arrange(secondary_data_latest_2025, validation_id)$name.7, arrange(secondary_modified_latest, validation_id)$`GFC validation - forest type`)   # FALSE; see remarks below
+
+
+
+### Some important remarks:  ####
+
+#  - In the 2025 dataset, the column names for the answers to the validation questions (e.g. "GFC validation - forest") are not modified; therefore they are all "name". When these are ingested to an R session, they are sequentially renamed to 'name.1', 'name.2', etc.  
+#  - In the 2025 dataset, `GFC validation - forest type` and `GFC validation - other land use type` are merged in one single column ('name.7'). This has to be kept in mind when reviewing the R scripts of the first assessment.
+# 
+
+
+
+
+# ---- 10) Issues by interpreter ----
+
+# Check for issues by interpreter. There should be nin, except for "no assignment" under "issues" for Europe, N-Africa, and S-Asia
+# This needs to be clarified with Rene
+## 
+
+
+View(secondary_modified_latest)
+
+
+
+# ---- 11) Replace "no assignment" with "no issue" under "issues" ----
+
+View(secondary_modified_latest)
+
+unique(secondary_data_latest_2025$name.5)
+table(sort(secondary_data_latest_2025$name.5))
+
+# cloud cover     forest to be regrown       low resolution   Multiple land uses        no assignment            no issues 
+#    14                  101                     53                     249                    1065                 2188 
+# no response data     Open treed land use         Other issues 
+#     118                   156                       56 
+
+table(sort(secondary_modified_latest$`GFC validation - Issues with class assignment`))
+sum(is.na(secondary_modified_latest$`GFC validation - Issues with class assignment`))   # 0 NAs
+
+
+secondary_modified_latest_adjusted <- secondary_modified_latest %>%
+  mutate(`GFC validation - Issues with class assignment` = 
+           if_else(`GFC validation - Issues with class assignment` == "no assignment",
+                   "no issues",
+                   `GFC validation - Issues with class assignment`))
+
+table(sort(secondary_modified_latest_adjusted$`GFC validation - Issues with class assignment`))
+
+
+
+write.csv(secondary_modified_latest_adjusted, 
+          paste0(dir, "secondary_data_latest_adjusted.csv"), 
+          row.names = FALSE)
+
+
+
+
+## some checks:
+
+secondary_data_latest_adjusted_2025 <- read.csv(paste0("/Users/xavi_rp/Documents/JRC_D1/Files_from_Geowiki_and_our_processing/", "secondary_data_latest_adjusted.csv"))
+
+sort(names(secondary_data_latest_adjusted_2025))
+nrow(secondary_data_latest_adjusted_2025) 
+
+head(secondary_data_latest_adjusted_2025)
+nrow(secondary_data_latest_adjusted_2025) == nrow(secondary_modified_latest_adjusted)  # TRUE
+
+sum(sort(unique(secondary_data_latest_adjusted_2025$validation_id)) != sort(unique(secondary_modified_latest_adjusted$validation_id)))     # 0
+
+identical(sort(unique(secondary_data_latest_adjusted_2025$validation_id)), sort(unique(secondary_modified_latest_adjusted$validation_id))) # TRUE
+identical(sort(unique(secondary_data_latest_adjusted_2025$sample_id)), sort(unique(secondary_modified_latest_adjusted$sample_id))) # TRUE
+
+identical(arrange(secondary_data_latest_adjusted_2025, validation_id)$name.1, arrange(secondary_modified_latest_adjusted, validation_id)$`GFC validation - forest`)   # TRUE
+
+
+
+
+
+
+
--- geowiki_output_processing.R
+++ geowiki_output_processing.R
@@ -1,3 +1,10 @@
+#-------------------------------------------------------------#
+#-------------------------------------------------------------#
+#------- Script to process the GeoWiki extracted data --------#
+#-------------------------------------------------------------#
+#-------------------------------------------------------------#
+
+
 
 library(tidyverse)
 
@@ -6,35 +13,35 @@ library(tidyverse)
 
 
 # ---- 0) Setup ----
-dir <- "/Users/xavi_rp/Documents/JRC_D1/AccuracyAssessment_Second/kk/"
 
-input_file <- "20260504.csv"
-input_file <- "20240905.csv"
+dir <- "/Users/xavi_rp/Documents/JRC_D1/AccuracyAssessment_Second/geowiki_2026/"
+
+input_file <- "2026_05_07_valgroup85_exports_GFC_2020.csv"
 
 input_file <- paste0(dir, input_file)
 
 
 # Extract date (YYYYMMDD)
-date_part <- tools::file_path_sans_ext(basename(input_file))
+date_part <- "20260507"
 
 
 
 
 # ---- 1) Read CSV (semicolon separated) ----
 
-input_file_df <- read.csv(input_file) # col 'name' is repeated. Transformed them into "name" and "name.1" 
+input_file_df <- read.csv(input_file, sep = ";") # col 'name' is repeated. Transformed them into "name" and "name.1" 
 
 
 head(input_file_df)
-nrow(input_file_df)
-ncol(input_file_df)
+nrow(input_file_df)  # 56007
+ncol(input_file_df)  # 18
 names(input_file_df)
 
 
 # ---- 2) Save initial copy ----
 
 write.csv(input_file_df, 
-          paste0(dir, date_part, "_GFC_2020_secondary.csv"), 
+          paste0(dir, date_part, "_GFC_2020.csv"), 
           row.names = FALSE)
 
 
@@ -79,7 +86,7 @@ if (checks != 0){
 # ---- 6) Save modified version ----
 
 write.csv(input_file_df, 
-          paste0(dir, date_part, "_GFC_2020_secondary_modified.csv"), 
+          paste0(dir, date_part, "_GFC_2020_modified.csv"), 
           row.names = FALSE)
 
 
@@ -87,19 +94,39 @@ write.csv(input_file_df,
 
 
 # ---- 7 & 8) Replace emails ----
- # to be clarified with Rene
-input_file_df <- input_file_df %>%
-  mutate(
-    email = case_when(
-      email == "rene.colditz@ec.europa.eu" ~ "danilo.mollicone@gmail.com",
-      email == "astrid.verhegghen@ec.europa.eu" ~ "valery.gond@cirad.fr",
-      TRUE ~ email
-    )
+# -Search for "rene.colditz@ec.europa.eu" in column "email" and replace with credentials from "danilo.mollicone@gmail.com". 
+#  This is to correct for a missing confidence assignment in this strata.
+# -Search for "astrid.verhegghen@ec.europa.eu" in column "email" and replace with credentials from "valery.gond@cirad.fr". 
+#  This is to correct for missing forest/non-forest and confidence assignments in this strata.
+
+
+    ### to be clarified with Rene ###
+
+
+# just to be sure if there are
+checks <- input_file_df %>%
+  summarise(
+    rene = sum(str_detect(email, "rene.colditz@ec.europa.eu"), na.rm = TRUE),
+    astrid = sum(str_detect(email, "astrid.verhegghen@ec.europa.eu"), na.rm = TRUE),
+    danilo = sum(str_detect(email, "danilo.mollicone@gmail.com"), na.rm = TRUE),
+    valery = sum(str_detect(email, "valery.gond@cirad.fr"), na.rm = TRUE)
   )
+
+checks
+
+
+#input_file_df <- input_file_df %>%
+#  mutate(
+#    email = case_when(
+#      email == "rene.colditz@ec.europa.eu" ~ "danilo.mollicone@gmail.com",
+#      email == "astrid.verhegghen@ec.europa.eu" ~ "valery.gond@cirad.fr",
+#      TRUE ~ email
+#    )
+#  )
 
 
 write.csv(input_file_df, 
-          paste0(dir, date_part, "_GFC_2020_secondary_modified.csv"), 
+          paste0(dir, date_part, "_GFC_2020_modified.csv"), 
           row.names = FALSE)
 
 
@@ -107,28 +134,31 @@ write.csv(input_file_df,
 
 
 # ---- 9) IDL code per_line_secondary.pro ----
+# IDL program translated into R
 
-secondary_modified <- read.csv(paste0(dir, date_part, "_GFC_2020_secondary_modified.csv"))
+paste0(date_part, "_GFC_2020_modified.csv")
 
-View(secondary_modified)
+#secondary_modified <- read.csv(paste0(dir, date_part, "_GFC_2020_modified.csv"))
+dataset_modified <- read.csv(paste0(dir, date_part, "_GFC_2020_modified.csv"))
 
-unique(secondary_modified$name)
-unique(secondary_modified$name.1)
+View(dataset_modified)
 
-length(unique(secondary_modified$validation_id))
-nrow(secondary_modified)
-nrow(secondary_modified)/length(unique(secondary_modified$validation_id))
+unique(dataset_modified$name)     
+unique(dataset_modified$name.1)
 
-table(secondary_modified$name)
+length(unique(dataset_modified$validation_id))  # 14019 validations
+nrow(dataset_modified)                          # total file rows (still several row per validation)
+
+table(dataset_modified$name)
 
 # check if there are duplicates per (validation_id, question). If 0 rows: each question appears exactly once per validation
-secondary_modified %>%
+dataset_modified %>%
   count(validation_id, name) %>%
   filter(n > 1)
 
 
 
-secondary_modified_wide <- secondary_modified %>%
+dataset_modified_wide <- dataset_modified %>%
   # make column names easier to work with
   rename(
     question = name,
@@ -148,25 +178,21 @@ secondary_modified_wide <- secondary_modified %>%
     .after = last_col()
   )
 
-View(secondary_modified_wide)
+View(dataset_modified_wide)
+nrow(dataset_modified_wide)
 
-nrow(secondary_modified_wide)
-length(unique(secondary_modified_wide$validation_id)) * 4
+head(sort(table(dataset_modified_wide$validation_id)), 10)
+tail(sort(table(dataset_modified_wide$validation_id)), 10)
+sum(table(dataset_modified_wide$validation_id) == 4)  # 13950
+sum(table(dataset_modified_wide$validation_id) == 3)  #    69
 
-nrow(secondary_modified_wide)/length(unique(secondary_modified_wide$validation_id))
-length(unique(secondary_modified_wide$validation_id)) * 4
-
-head(sort(table(secondary_modified_wide$validation_id)), 10)
-sum(table(secondary_modified_wide$validation_id) == 4)  # 4233
-sum(table(secondary_modified_wide$validation_id) == 3)  #   80
-
-secondary_modified_wide[secondary_modified_wide$validation_id == 2745966, ] %>% View() # if 'GFC validation - forest' = 'no assignment', it has only 3 rows
-secondary_modified_wide[secondary_modified_wide$validation_id == 2748197, ] %>% View() 
+dataset_modified_wide[dataset_modified_wide$validation_id == 2936848, ] %>% View() # if 'GFC validation - forest' = 'no assignment', it has only 3 rows
+dataset_modified_wide[dataset_modified_wide$validation_id == 2959057, ] %>% View() 
 
 
 
 ### Collapsing the rows into a single row per validation_id, merging the non-NA values ####
-secondary_modified_wide_1row <- secondary_modified_wide %>%
+dataset_modified_wide_1row <- dataset_modified_wide %>%
   group_by(validation_id) %>%
   summarise(
     across(starts_with("GFC"),
@@ -176,16 +202,16 @@ secondary_modified_wide_1row <- secondary_modified_wide %>%
   relocate(starts_with("GFC"), .after = last_col()) %>%
   relocate(comment, .after = last_col())
 
-View(secondary_modified_wide_1row)
+View(dataset_modified_wide_1row)
 
 
-secondary_modified_wide_1row[secondary_modified_wide_1row$validation_id == 2745966, ] %>% View() 
-secondary_modified_wide_1row[secondary_modified_wide_1row$validation_id == 2748197, ] %>% View() 
+dataset_modified_wide_1row[dataset_modified_wide_1row$validation_id == 2936848, ] %>% View() 
+dataset_modified_wide_1row[dataset_modified_wide_1row$validation_id == 2959057, ] %>% View() 
 
 
 
-write.csv(secondary_modified_wide_1row, 
-          paste0(dir, "secondary_data.csv"), 
+write.csv(dataset_modified_wide_1row, 
+          paste0(dir, date_part, "_data.csv"), 
           row.names = FALSE)
 
 
@@ -196,64 +222,42 @@ write.csv(secondary_modified_wide_1row,
 
 ## are there samples which have several validations?
 
-length(unique(secondary_modified_wide$validation_id)) # 4313, because some sample units were validated more than once
-length(unique(secondary_modified_wide$sample_id))     # 4000
+length(unique(dataset_modified_wide$validation_id)) # 14019, because some sample units were validated more than once
+length(unique(dataset_modified_wide$sample_id))     # 13764, which is the number of sample units to be validated in this round
+                                                    # correct
 
 
 
-secondary_modified_latest <- secondary_modified_wide_1row %>%
+dataset_modified_latest <- dataset_modified_wide_1row %>%
   group_by(sample_id) %>%
   slice_max(validation_id, n = 1, with_ties = FALSE) %>%
   ungroup()
 
-View(secondary_modified_latest)
+View(dataset_modified_latest)
+nrow(dataset_modified_latest)   # 13764
 
 
 
-write.csv(secondary_modified_latest, 
-          paste0(dir, "secondary_data_latest.csv"), 
+write.csv(dataset_modified_latest, 
+          paste0(dir, date_part, "_data_latest.csv"), 
           row.names = FALSE)
 
 
 
 ### Some checks ####
 
-View(secondary_modified[secondary_modified$sample_id == 2027527, ])
-View(secondary_modified_wide[secondary_modified_wide$sample_id == 2027527, ])
-View(secondary_modified_wide_1row[secondary_modified_wide_1row$sample_id == 2027527, ])  # info from "submission_item_id" and "enhancement" are lost, but they seem not relevant
-View(secondary_modified_latest[secondary_modified_latest$sample_id == 2027527, ])
+View(dataset_modified[dataset_modified$sample_id == 2229436, ])
+View(dataset_modified_wide[dataset_modified_wide$sample_id == 2229436, ])
+View(dataset_modified_wide_1row[dataset_modified_wide_1row$sample_id == 2229436, ])  # info from "submission_item_id" and "enhancement" are lost (only one remain), but they seem not relevant (each question/answer has its own "submission_item_id" and "enhancement")
+View(dataset_modified_latest[dataset_modified_latest$sample_id == 2229436, ])
 
 
 
-### Checking against published version (2025) of "secondary_data_latest.csv" ####
-
-secondary_data_latest_2025 <- read.csv(paste0("/Users/xavi_rp/Documents/JRC_D1/Files_from_Geowiki_and_our_processing/", "secondary_data_latest.csv"))
-
-sort(names(secondary_data_latest_2025))
-View(secondary_data_latest_2025)
-nrow(secondary_data_latest_2025) 
-
-head(secondary_modified_latest)
-nrow(secondary_data_latest_2025) == nrow(secondary_modified_latest)  # 4000
-
-sum(sort(unique(secondary_data_latest_2025$validation_id)) != sort(unique(secondary_modified_latest$validation_id)))     # 0
-identical(sort(unique(secondary_data_latest_2025$validation_id)), sort(unique(secondary_modified_latest$validation_id))) # TRUE
-
-identical(sort(unique(secondary_data_latest_2025$sample_id)), sort(unique(secondary_modified_latest$sample_id))) # TRUE
-
-sort(names(secondary_modified_latest))
-View(secondary_modified_latest)
-
-identical(arrange(secondary_data_latest_2025, validation_id)$name.1, arrange(secondary_modified_latest, validation_id)$`GFC validation - forest`)   # TRUE
-
-identical(arrange(secondary_data_latest_2025, validation_id)$name.7, arrange(secondary_modified_latest, validation_id)$`GFC validation - forest type`)   # FALSE; see remarks below
 
 
-
-### Some important remarks:  ####
-
-#  - In the 2025 dataset, the column names for the answers to the validation questions (e.g. "GFC validation - forest") are not modified; therefore they are all "name". When these are ingested to an R session, they are sequentially renamed to 'name.1', 'name.2', etc.  
-#  - In the 2025 dataset, `GFC validation - forest type` and `GFC validation - other land use type` are merged in one single column ('name.7'). This has to be kept in mind when reviewing the R scripts of the first assessment.
+### Some important remarks:  ###
+#  - In the 2024 dataset, the column names for the answers to the validation questions (e.g. "GFC validation - forest") are not modified; therefore they are all "name". When these are ingested to an R session, they are sequentially renamed to 'name.1', 'name.2', etc.  
+#  - In the 2024 dataset, `GFC validation - forest type` and `GFC validation - other land use type` are merged in one single column ('name.7'). This has to be kept in mind when reviewing the R scripts of the first assessment.
 # 
 
 
@@ -266,61 +270,37 @@ identical(arrange(secondary_data_latest_2025, validation_id)$name.7, arrange(sec
 ## 
 
 
-View(secondary_modified_latest)
+View(dataset_modified_latest)
 
 
 
 # ---- 11) Replace "no assignment" with "no issue" under "issues" ----
 
-View(secondary_modified_latest)
+View(dataset_modified_latest)
 
-unique(secondary_data_latest_2025$name.5)
-table(sort(secondary_data_latest_2025$name.5))
-
-# cloud cover     forest to be regrown       low resolution   Multiple land uses        no assignment            no issues 
-#    14                  101                     53                     249                    1065                 2188 
-# no response data     Open treed land use         Other issues 
-#     118                   156                       56 
-
-table(sort(secondary_modified_latest$`GFC validation - Issues with class assignment`))
-sum(is.na(secondary_modified_latest$`GFC validation - Issues with class assignment`))   # 0 NAs
+table(sort(dataset_modified_latest$`GFC validation - Issues with class assignment`))
+sum(dataset_modified_latest$`GFC validation - Issues with class assignment` == "no assignment", na.rm = TRUE)  # 97
+sum(is.na(dataset_modified_latest$`GFC validation - Issues with class assignment`))   # 0 NAs
 
 
-secondary_modified_latest_adjusted <- secondary_modified_latest %>%
+dataset_modified_latest_adjusted <- dataset_modified_latest %>%
   mutate(`GFC validation - Issues with class assignment` = 
            if_else(`GFC validation - Issues with class assignment` == "no assignment",
                    "no issues",
                    `GFC validation - Issues with class assignment`))
 
-table(sort(secondary_modified_latest_adjusted$`GFC validation - Issues with class assignment`))
+table(sort(dataset_modified_latest_adjusted$`GFC validation - Issues with class assignment`))
+sum(dataset_modified_latest_adjusted$`GFC validation - Issues with class assignment` == "no assignment", na.rm = TRUE)  # 0
 
 
 
-write.csv(secondary_modified_latest_adjusted, 
-          paste0(dir, "secondary_data_latest_adjusted.csv"), 
+write.csv(dataset_modified_latest_adjusted, 
+          paste0(dir, date_part, "data_latest_adjusted.csv"), 
           row.names = FALSE)
 
 
-
-
-## some checks:
-
-secondary_data_latest_adjusted_2025 <- read.csv(paste0("/Users/xavi_rp/Documents/JRC_D1/Files_from_Geowiki_and_our_processing/", "secondary_data_latest_adjusted.csv"))
-
-sort(names(secondary_data_latest_adjusted_2025))
-nrow(secondary_data_latest_adjusted_2025) 
-
-head(secondary_data_latest_adjusted_2025)
-nrow(secondary_data_latest_adjusted_2025) == nrow(secondary_modified_latest_adjusted)  # TRUE
-
-sum(sort(unique(secondary_data_latest_adjusted_2025$validation_id)) != sort(unique(secondary_modified_latest_adjusted$validation_id)))     # 0
-
-identical(sort(unique(secondary_data_latest_adjusted_2025$validation_id)), sort(unique(secondary_modified_latest_adjusted$validation_id))) # TRUE
-identical(sort(unique(secondary_data_latest_adjusted_2025$sample_id)), sort(unique(secondary_modified_latest_adjusted$sample_id))) # TRUE
-
-identical(arrange(secondary_data_latest_adjusted_2025, validation_id)$name.1, arrange(secondary_modified_latest_adjusted, validation_id)$`GFC validation - forest`)   # TRUE
-
-
+View(dataset_modified_latest_adjusted)
+nrow(dataset_modified_latest_adjusted) # 13764
 
 
 

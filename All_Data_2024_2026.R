fd474c36438106b4d0d93b0b3f06ad7199205703
xavi-rp <xavi.rotllan.puig@gmail.com>
2026-06-08 12:25:05 +0200

    2026 1st roound validations ready

--- All_Data_2024_2026.R
+++ All_Data_2024_2026.R
@@ -31,7 +31,7 @@ primary_data_latest <- read.csv(paste0(dir_old_results, "primary_data_latest.csv
 
 nrow(primary_data_latest) # 21752
 names(primary_data_latest) 
-View(primary_data_latest[1:10, ]) # 21752
+#View(primary_data_latest[1:10, ]) # 21752
 
 
 valid_2024_1st <- primary_data_latest %>% 
@@ -61,7 +61,7 @@ valid_2024_1st <- primary_data_latest %>%
     ) #%>% 
 
 nrow(valid_2024_1st)  # 21752
-View(valid_2024_1st[1:10, ]) # 21752
+#View(valid_2024_1st[1:10, ]) # 21752
 
 apply(valid_2024_1st, 2, function(x) sum(is.na(x)))
 #unique(valid_2024_1st$X2024_1st_comment)
@@ -74,7 +74,7 @@ secondary_data_latest <- read.csv(paste0(dir_old_results, "secondary_data_latest
 
 nrow(secondary_data_latest) # 4000
 names(secondary_data_latest) 
-View(secondary_data_latest[1:10, ]) 
+#View(secondary_data_latest[1:10, ]) 
 
 
 valid_2024_2nd <- secondary_data_latest %>% 
@@ -104,7 +104,7 @@ valid_2024_2nd <- secondary_data_latest %>%
   ) #%>% 
 
 nrow(valid_2024_2nd)  # 4000
-View(valid_2024_2nd[1:10, ]) 
+#View(valid_2024_2nd[1:10, ]) 
 
 apply(valid_2024_2nd, 2, function(x) sum(is.na(x)))
 
@@ -128,18 +128,46 @@ valid_2024_final <- full_2024_recreated %>%
 
 head(valid_2024_final)
 
-
+sort(unique(valid_2024_final$X2024_final_groupid))
 
 
 
 ## 2026 first round ####
 
+data_2026_2024_clean <- read.csv(paste0(dir_geowiki, date_part, "_comparison_2026_2024_clean.csv"))
 
 
+data_2026_2024_clean[1:2, ]
 
 
+valid_2026_1st <- data_2026_2024_clean %>% 
+  select(location_id, userid, email, groupid, sample_id,  
+         starts_with("GFC."), comment) %>%    # pull(groupid) %>% unique() %>% sort()
+  mutate(type_class = coalesce(GFC.validation...forest.type, GFC.validation...other.land.use.type)) %>% #head()
+  select(-GFC.validation...forest.type, -GFC.validation...other.land.use.type) %>% 
+  rename(
+    "X2026_1st_userid" = "userid",
+    "X2026_1st_email" = "email",
+    "X2026_1st_groupid" = "groupid",
+    "X2026_1st_sample_id" = "sample_id",
+    "X2026_1st_forest_class" = "GFC.validation...forest",
+    "X2026_1st_confidence_level" = "GFC.validation...confidence",
+    "X2026_1st_type_class" = "type_class",
+    "X2026_1st_class_issues" = "GFC.validation...Issues.with.class.assignment",
+    "X2026_1st_comment" = "comment"
+  ) %>%
+  relocate(X2026_1st_type_class, .before = X2026_1st_class_issues) #%>% head()
 
-#
+
+valid_2026_1st[1:2, ]
+nrow(valid_2026_1st)   # 13764
+apply(valid_2026_1st, 2, function(x) sum(is.na(x))) # 13 that have missing values in type_class, but that are reingested in 2026 2nd round
+
+#sum(valid_2026_1st %>% filter(is.na(X2026_1st_type_class)) %>% pull(location_id) %in%
+#  scenario2_plusMissing$location_id)  # 13 that have missing values in type_class, but that are reingested in 2026 2nd round
+
+sum(!valid_2026_1st$location_id %in% valid_2024_final$location_id)  # 11 which couldn't be assigned in the 2024 final round
+sum(!valid_2026_1st$location_id %in% valid_2024_1st$location_id)  # 0
 
 
 
@@ -151,22 +179,46 @@ valid_2024_1st %>% head()
 valid_2024_2nd %>% head()
 valid_2024_1st %>% nrow()  # 21752
 valid_2024_2nd %>% nrow()  #  4000
+valid_2026_1st %>% nrow()  #  13764
 unique(valid_2024_1st$location_id) %>% length()
 unique(valid_2024_2nd$location_id) %>% length()
+unique(valid_2026_1st$location_id) %>% length()
 
 
 valid_all <- valid_2024_1st %>% 
   full_join(valid_2024_2nd, by = "location_id") %>%
-  #  full_join(valid_2024_2nd, by = "location_id") %>%  nrow()
   relocate(location_id) %>% 
-  full_join(valid_2024_final, by = "location_id")
+  full_join(valid_2024_final, by = "location_id") %>% 
+  full_join(valid_2026_1st, by = "location_id")
 
 
 valid_all %>% head()
 valid_all %>% nrow()  # 21752
 apply(valid_all, 2, function(x) sum(is.na(x)))
 
+user_check_1 <- valid_all %>% 
+  select(X2024_1st_userid, X2024_1st_email) %>% 
+  distinct() %>%
+  arrange(X2024_1st_userid)
+
+user_check_2 <- valid_all %>% 
+  select(X2024_2nd_userid, X2024_2nd_email) %>% 
+  distinct() %>%
+  arrange(X2024_2nd_userid)
+
+user_check_3 <- valid_all %>% 
+  select(X2026_1st_userid, X2026_1st_email) %>% 
+  distinct() %>%
+  arrange(X2026_1st_userid)
 
 
 
+user_check_1 %>% 
+  full_join(user_check_2, by = c("X2024_1st_userid" = "X2024_2nd_userid")) %>% 
+  full_join(user_check_3, by = c("X2024_1st_userid" = "X2026_1st_userid"))
+
+## Note that some of the users email corresponds to another actual caller 
+## e.g. 'xavi.rotllan.puig@gmail.com' strata was made by 'silvia.carboni@ext.ec.europa.eu'
+## using Xavi's credentials.
+## The rest should be checked with Rene
 
